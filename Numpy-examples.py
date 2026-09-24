965863a042dd3e664d139a85d37b9cad93b9ca8c
daryl <daryl@shepherdson.com>
2026-09-24 21:36:29 +0800

    Deleted useless files

--- Numpy-examples.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a1 = np.array([1,2,3,4,5])
-print(a1[3])
--- import numpy as np.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-a1 = np.array([1, 2, 3, 4, 5])
-print(a1[3])  # This will raise an IndexError since the array has only
